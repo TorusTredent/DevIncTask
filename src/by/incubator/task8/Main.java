cb6c99950cfca68db0d44ba5655a19ceab989fad
TorusTredent <torus.tredent@gmail.com>
2022-10-07 19:02:13 +0300

    complete task10

--- src/by/incubator/task8/Main.java
+++ src/by/incubator/task8/Main.java
@@ -3,7 +3,7 @@ package by.incubator.task8;
 public class Main {
 
     public static void main(String[] args) {
-//        int[] arr = new int[99999];
+        int[] arr = new int[99999];
         test(1);
     }
 
